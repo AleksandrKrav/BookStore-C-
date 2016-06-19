c9c516d0f483456fb815127d03e63deae32be111
Alex <sanya.kravchuk@ukr.net>
2016-06-19 14:06:19 +0300

    C# bookstore

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Threading.Tasks;
+using System.Data.Entity;
+using BookStore.Models;
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
+            return View();
+        }
+
+        // асинхронный метод
+        public async Task<ActionResult> BookList()
+        {
+            IEnumerable<Book> books = await db.Books.ToListAsync();
+            ViewBag.Books = books;
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+
+            db.Purchases.Add(purchase);
+
+            db.SaveChanges();
+
+            return "Thanks," + purchase.Person + ", for order!";
+        }
+
+        public string Square(int a = 10, int h = 3)
+        {
+            double s = a * h / 2;
+            return "<h2>Площадь треугольника с основанием " + a +
+                    " и высотой " + h + " равна " + s + "</h2>";
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BookStore/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public string Index()        
+            {
+                string browser = HttpContext.Request.Browser.Browser;
+                string user_agent = HttpContext.Request.UserAgent;
+                string url = HttpContext.Request.RawUrl;
+                string ip = HttpContext.Request.UserHostAddress;
+                string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+                return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                    "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
+            }        
+	}
+}
